ee12d1fc24ac38e29adb349390fe4a287a9b9f8a
Maiguel <miguel1235.mar@gmail.com>
2024-12-17 14:49:59 -0500

    Change Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,10 @@
 import LogIn from "../components/auth/LogIn";
 
-const Login = () => {
+export default function Login() {
   return (
     <div className="f-center h-[100vh] w-full bg-[url('/src/assets/books_bg.jpeg')] pt-[2rem]">
       <div className="absolute left-0 top-0 h-full w-full bg-black opacity-20"></div>
       <LogIn />
     </div>
   );
-};
-
-export default Login;
+}
