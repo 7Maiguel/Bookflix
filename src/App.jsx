61139f2a24acc68522b717862dda151b5716e0b6
Maiguel <miguel1235.mar@gmail.com>
2024-12-17 14:54:18 -0500

    Change logic of login feature to use React contexts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import { GlobalContext } from "./contexts";
+import useLocalStorage from "./hooks/localstorage";
 import NavBar from "./components/nav/NavBar";
 import NotFound from "./pages/NotFound";
 import Genres from "./pages/Genres";
@@ -8,16 +10,39 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 export function App() {
+  const [sessionLoggedIn, setSessionLoggedIn] = useLocalStorage(
+    "logged_in",
+    false,
+  );
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(sessionLoggedIn);
+
+  useEffect(() => {
+    setSessionLoggedIn(isUserLoggedIn);
+  }, [isUserLoggedIn]);
+
+  useEffect(() => {});
+
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/generos" element={<Genres />}></Route>
-        <Route path="/perfil" element={<Profile />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <GlobalContext.Provider value={{ isUserLoggedIn, setIsUserLoggedIn }}>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route
+            path="/"
+            element={isUserLoggedIn ? <Home /> : <Login />}
+          ></Route>
+          <Route
+            path="/generos"
+            element={isUserLoggedIn ? <Genres /> : <Login />}
+          ></Route>
+          <Route
+            path="/perfil"
+            element={isUserLoggedIn ? <Profile /> : <Login />}
+          ></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </GlobalContext.Provider>
   );
 }
--- src/components/auth/LogIn.jsx
+++ src/components/auth/LogIn.jsx
@@ -1,22 +1,38 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../contexts";
+import { useNavigate } from "react-router-dom";
 import useLocalStorage from "../../hooks/localstorage";
 
-const LogIn = () => {
-  // User Login
-  const handleSubmit = (e) => {
+export default function LogIn() {
+  const [user] = useLocalStorage("user", null);
+  const { setIsUserLoggedIn } = useContext(GlobalContext);
+  const navigate = useNavigate();
+
+  const validateUserAuth = (e) => {
     e.preventDefault();
-    const payload = { email: e.target[0].value, password: e.target[1].value };
-    useLocalStorage("user", JSON.stringify(payload));
-    location = "/";
+    if (
+      e.target.email.value === user.email &&
+      e.target.password.value === user.password
+    ) {
+      setIsUserLoggedIn(true);
+      navigate("/");
+    } else {
+      alert("Email o contraseña incorrectos");
+    }
   };
 
   return (
     <div className="relative flex w-[35%] flex-col rounded bg-gray-200 px-6 pb-7 pt-5 text-black">
-      <h1 className="text-3xl font-semibold">Inicio de sesión</h1>
+      <h1 className="text-3xl font-semibold">¡Bienvenido!</h1>
 
-      <form className="my-4" onSubmit={handleSubmit}>
+      <form className="my-4" onSubmit={validateUserAuth}>
         <input type="text" name="email" placeholder="Email" />
         <input type="text" name="password" placeholder="Contraseña" />
-        <button type="submit" className="primary-btn w-full shadow-lg">
+        <button
+          type="submit"
+          className="primary-btn w-full shadow-lg"
+          disabled={!user}
+        >
           Ingresar
         </button>
       </form>
@@ -24,6 +40,4 @@ const LogIn = () => {
       <button className="secondary-btn w-full shadow-lg">Registrarme</button>
     </div>
   );
-};
-
-export default LogIn;
+}
