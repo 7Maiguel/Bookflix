98df26b5da1dfb5d8d532d4ba8b35958d2955c83
Maiguel <miguel1235.mar@gmail.com>
2024-12-24 08:26:49 -0500

    Add conditional render to show sign up or login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import LogIn from "../components/auth/LogIn";
+import SignUp from "../components/auth/SignUp";
 
 export default function Login() {
+  const [signingUp, setSigningUp] = useState(false);
+
   return (
     <div className="f-center h-[100vh] w-full bg-[url('/src/assets/books_bg.jpeg')] pt-[2rem]">
       <div className="absolute left-0 top-0 h-full w-full bg-black opacity-20"></div>
-      <LogIn />
+      <div className="relative flex w-[35%] flex-col rounded bg-gray-200 px-6 py-7 pt-5 text-black">
+        <h1 className="mb-1 text-3xl font-semibold">
+          {signingUp ? "¿Eres nuevo por aquí?" : "¡Bienvenido!"}
+        </h1>
+        {signingUp ? (
+          <SignUp openLogInForm={() => setSigningUp(false)} />
+        ) : (
+          <LogIn openSignUpForm={() => setSigningUp(true)} />
+        )}
+      </div>
     </div>
   );
 }
