c1aeb5440e8efb13b9e3e8347d6817493d96537d
Maiguel <miguel1235.mar@gmail.com>
2024-12-24 08:28:20 -0500

    Add navigation to sign up button

--- src/components/auth/LogIn.jsx
+++ src/components/auth/LogIn.jsx
@@ -1,18 +1,18 @@
 import { useContext } from "react";
-import { GlobalContext } from "../../contexts";
 import { useNavigate } from "react-router-dom";
+import { GlobalContext } from "../../contexts";
 import useLocalStorage from "../../hooks/localstorage";
 
-export default function LogIn() {
-  const [user] = useLocalStorage("user", null);
+export default function LogIn({ openSignUpForm }) {
+  const [sessionUser] = useLocalStorage("user", null);
   const { setIsUserLoggedIn } = useContext(GlobalContext);
   const navigate = useNavigate();
 
   const validateUserAuth = (e) => {
     e.preventDefault();
     if (
-      e.target.email.value === user.email &&
-      e.target.password.value === user.password
+      e.target.email.value === sessionUser.email &&
+      e.target.password.value === sessionUser.password
     ) {
       setIsUserLoggedIn(true);
       navigate("/");
@@ -22,22 +22,23 @@ export default function LogIn() {
   };
 
   return (
-    <div className="relative flex w-[35%] flex-col rounded bg-gray-200 px-6 pb-7 pt-5 text-black">
-      <h1 className="text-3xl font-semibold">¡Bienvenido!</h1>
-
-      <form className="my-4" onSubmit={validateUserAuth}>
-        <input type="text" name="email" placeholder="Email" />
-        <input type="text" name="password" placeholder="Contraseña" />
-        <button
-          type="submit"
-          className="primary-btn w-full shadow-lg"
-          disabled={!user}
-        >
-          Ingresar
-        </button>
-      </form>
-
-      <button className="secondary-btn w-full shadow-lg">Registrarme</button>
-    </div>
+    <form className="mt-4" onSubmit={validateUserAuth}>
+      <input type="email" name="email" placeholder="Correo Electrónico" />
+      <input type="text" name="password" placeholder="Contraseña" required />
+      <button
+        type="submit"
+        className="primary-btn my-2 w-full shadow-lg"
+        disabled={!sessionUser}
+      >
+        Ingresar
+      </button>
+      <button
+        type="button"
+        className="secondary-btn w-full shadow-lg"
+        onClick={openSignUpForm}
+      >
+        Registrarme
+      </button>
+    </form>
   );
 }
