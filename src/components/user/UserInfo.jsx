14017eb501bcf1987b20287191588ae7de5e3296
Maiguel <miguel1235.mar@gmail.com>
2024-12-26 15:03:04 -0500

    Change data source to use local storage hook

--- src/components/user/UserInfo.jsx
+++ src/components/user/UserInfo.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
+import useLocalStorage from "../../hooks/localstorage";
 import { censorString } from "../../utils/strings";
 import CustomIcon from "../common/CustomIcon";
 
-export default function UserInfo({ userData, activeEditingMode }) {
+export default function UserInfo() {
+  const [sessionUser] = useLocalStorage("user", null);
   const [censoringData, setCensoringData] = useState(true);
 
   return (
-    <>
+    <div className="flex h-[320px] w-[55%]">
       <img
         className="mr-6 h-full w-[280px] rounded border-2 border-white object-cover"
         src="https://avatarfiles.alphacoders.com/203/thumb-1920-203661.jpg"
@@ -15,14 +17,14 @@ export default function UserInfo({ userData, activeEditingMode }) {
       <div className="relative w-[60%]">
         <b>Nombre Completo</b>
         <br />
-        <span className="text-xl">{userData.fullName}</span>
+        <span className="text-xl">{sessionUser.fullName}</span>
         <br />
         <br />
         <b>Correo Electrónico</b>
         <br />
         <div className="flex items-center">
           <span className="text-xl">
-            {censoringData ? censorString(userData.email, 5) : userData.email}
+            {censoringData ? censorString(sessionUser.email, 5) : sessionUser.email}
           </span>
           <CustomIcon
             type={censoringData ? "eye" : "eyeOff"}
@@ -37,6 +39,6 @@ export default function UserInfo({ userData, activeEditingMode }) {
           <span className="material-symbols-outlined ml-2">edit</span>
         </button> */}
       </div>
-    </>
+    </div>
   );
 }
