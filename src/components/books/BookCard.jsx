ab6bc384f5f344ab4ab0ddf8c79bab5a8c39ed72
Maiguel <miguel1235.mar@gmail.com>
2025-01-09 13:55:59 -0500

    Add new logic to handle like interactions from book list instead of book cards

--- src/components/books/BookCard.jsx
+++ src/components/books/BookCard.jsx
@@ -1,24 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import CustomIcon from "../common/CustomIcon";
-import useLocalStorage from "../../hooks/localstorage";
 
-export default function BookCard({ bookId, cover }) {
+export default function BookCard({ bookId, cover, isSaved, handleLike }) {
   const [mouseFocus, setMouseFocus] = useState(false);
-  const [favoriteBooks, setFavoriteBooks] = useLocalStorage(
-    "favorite_books",
-    [],
-  );
-  const [bookSaved, setBookSaved] = useState(favoriteBooks.includes(bookId));
 
   const toggleMouseFocus = () => setMouseFocus(!mouseFocus);
-
-  const toggleLikeBook = () => {
-    setBookSaved(!bookSaved);
-
-    if (!favoriteBooks.includes(bookId))
-      setFavoriteBooks([...favoriteBooks, bookId]);
-    else setFavoriteBooks(favoriteBooks.filter((i) => i != bookId));
-  };
 
   return (
     <div
@@ -32,9 +18,9 @@ export default function BookCard({ bookId, cover }) {
       />
       {mouseFocus && (
         <CustomIcon
-          type={bookSaved ? "favoriteFilled" : "favorite"}
+          type={isSaved ? "favoriteFilled" : "favorite"}
           styles="text-3xl absolute top-2 right-2"
-          handleClick={toggleLikeBook}
+          handleClick={() => handleLike(bookId)}
         />
       )}
     </div>
--- src/components/books/BookList.jsx
+++ src/components/books/BookList.jsx
@@ -1,11 +1,44 @@
+import { useEffect, useState } from "react";
 import booksData from "../../db/books.json";
+import useLocalStorage from "../../hooks/localstorage";
 import BookCard from "./BookCard";
 
 export default function BookList() {
+  const [favoriteBooksIDS, setFavoriteBooksIDS] = useLocalStorage(
+    "favorite_books",
+    [],
+  );
+  const [favoriteBooks, setFavoriteBooks] = useState([]);
+
+  useEffect(() => {
+    setFavoriteBooks(
+      booksData.filter((book) => favoriteBooksIDS.includes(book.id)),
+    );
+  }, []);
+
+  const toggleLike = (bookId) => {
+    if (favoriteBooksIDS.includes(bookId)) {
+      setFavoriteBooksIDS(favoriteBooksIDS.filter((id) => id !== bookId));
+      setFavoriteBooks(favoriteBooks.filter((book) => book.id !== bookId));
+    } else {
+      setFavoriteBooksIDS([...favoriteBooksIDS, bookId]);
+      setFavoriteBooks([
+        ...favoriteBooks,
+        booksData.find((book) => book.id === bookId),
+      ]);
+    }
+  };
+
   return (
     <div className="grid w-full grid-cols-7 gap-10">
       {booksData.map((book) => (
-        <BookCard key={book.id} bookId={book.id} cover={book.cover} />
+        <BookCard
+          key={book.id}
+          bookId={book.id}
+          cover={book.cover}
+          isSaved={favoriteBooksIDS.includes(book.id)}
+          handleLike={toggleLike}
+        />
       ))}
     </div>
   );
